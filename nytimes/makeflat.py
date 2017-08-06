ac5de6675fd59ff60fa64c9b03b6371a3bcc043b
Sabine Maennel <sabine.maennel@gmail.com>
2017-08-06 22:34:46 +0200

    docu of makeflat.py updated

--- nytimes/makeflat.py
+++ nytimes/makeflat.py
@@ -1,13 +1,35 @@
 # -*- coding: utf-8 -*-
+"""
+This file contains recursive functions that allow
+to flatten nested datastructures that are build
+with dictionaries and lists
 
+Examples:
+
+{'headline': {'main': 'some headline', 'kicker': 'some excitement'}}
+is turned into:
+{'headline.main': 'some headline',
+'headline.kicker': 'some excitement'}
+
+{'keywords': [{'value': 'US', rank': 1}, {'value': 'Soccer', rank': 2}]}
+is tunred into:
+{'keywords.0.value': 'US',
+ 'keywords.0.rank': 1,
+ 'keywords.1.value': 'Soccer',
+ 'keywords.1.rank': 2}
+```
+"""
 from collections import OrderedDict
 
 
 def get_structure_depth(structure):
     """determine the depth of a structure:
+
     - assume the most complex case and reduce the complexity by 1 degree
-    the structure might be a dictionary or list, in this case the depth is the
-    the depth of the items + 1
+
+    - the structure might be a dictionary or list, in this case the depth is the
+    the depth of the nested items + 1
+
     - catch the case where the depth is 0"""
     if isinstance(structure, dict) and structure:
         return max([get_structure_depth(v) for v in structure.values()]) + 1
@@ -18,18 +40,25 @@ def get_structure_depth(structure):
 
 
 def make_flat_structure(structure):
-    """makes a structure flat
+    """makes a structure flat:
+
     - if the structure is basic: it is returned as is
-    - if the structure is a list or dict, a list of tuples is
-    produced: struct_items can be [(k,v) for (k,v) in structure.items()]
+
+    - if the structure is a list or dict,
+    a list of tuples is produced:
+
+    struct_items can be [(k,v) for (k,v) in structure.items()]
     if the struture is a dict
-    or [('0', l[0]), ('1', l(1)), ...] if the struture is a list
+
+    or [('0', l[0]), ('1', l(1)), ...] if the structure is a list
+
     now values can be made into a flat structure by recursion
     'value_flat_structure' and keys are taken to get the new names,
     which are 'k.key(value_flat_structure)'
 
-    by using an Ordered Dict to flatten the structure, order of values is
-    retained, that is especially useful for flattening lists.
+    by using an Ordered Dict to flatten the structure, the order
+    of values is retained
+    - that is especially useful for flattening lists.
     """
     if get_structure_depth(structure) <= 1:
         return structure
